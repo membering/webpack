d152681c9aa080c9cecf912541ba7d413c52d1e7
HOME <trieuthieuvan290590@gmail.com>
2016-12-06 00:58:43 +0700

    import infor from contact list when send card

--- src/app/components/home/card/action/send.component.ts
+++ src/app/components/home/card/action/send.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { CardService, AlertService } from '../../../../services/index';
+import { CardService, ContactService, AlertService } from '../../../../services/index';
 
 @Component({
     templateUrl: 'send.component.html'
@@ -12,11 +12,14 @@ export class SendComponent implements OnInit {
     loading = false;
     model: any = {};
     data: any = [];
+    listContact: any = [];
+    contact: any = [];
 
     constructor(
         private router: Router,
         private route: ActivatedRoute,
         private cardService: CardService,
+        private contactService: ContactService,
         private alertService: AlertService
     ) {
         this.title = route.snapshot.data['name'];
@@ -25,10 +28,15 @@ export class SendComponent implements OnInit {
 
     ngOnInit() {
         this.loadDetail();
+        this.loadContact();
     }
 
     loadDetail() {
         return this.cardService.getDetail(this.params['id']).subscribe(response => this.data = response.data);
+    }
+
+    loadContact() {
+        return this.contactService.getList(1).subscribe(response => this.listContact = response.data);
     }
 
     onSubmit() {
@@ -50,5 +58,19 @@ export class SendComponent implements OnInit {
                     this.alertService.error(error);
                     this.loading = false;
                 });
+    }
+
+    selectContact(options) {
+        this.contact = Array.apply(null, options)  // convert to real Array
+            .filter(option => option.selected)
+            .map((v, i) => {
+                console.log(v.value);
+                this.model.email = this.listContact[v.value].email;
+                this.model.phone = this.listContact[v.value].phone;
+            });
+        // console.log(options);
+        // this.contact.map((v, i) => {
+        //
+        // })
     }
 }
